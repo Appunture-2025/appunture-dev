9c2b69f43b4adeb1607fc2d9c9b43f0e2dd658ea
voidalzk <voidalzk@gmail.com>
2025-12-01 20:42:52 -0300

    update dos manifests

--- frontend-mobile/appunture/assets/body-map/manifest.ts
+++ frontend-mobile/appunture/assets/body-map/manifest.ts
@@ -22,21 +22,46 @@ const makeLayer = (
 export const BODY_ATLAS_LAYERS: BodyAtlasLayer[] = [
   // 12 Meridianos Primários
   makeLayer("LU", "Lung · Pulmão", "front", require("./LU.svg")),
-  makeLayer("LI", "Large Intestine · Intestino Grosso", "front", require("./LI.svg")),
+  makeLayer(
+    "LI",
+    "Large Intestine · Intestino Grosso",
+    "front",
+    require("./LI.svg")
+  ),
   makeLayer("ST", "Stomach · Estômago (1)", "front", require("./ST.svg")),
   makeLayer("ST2", "Stomach · Estômago (2)", "front", require("./ST2.svg")),
   makeLayer("SP", "Spleen · Baço", "front", require("./SP.svg")),
   makeLayer("HT", "Heart · Coração", "front", require("./HT.svg")),
-  makeLayer("SI", "Small Intestine · Intestino Delgado", "back", require("./SI.svg")),
+  makeLayer(
+    "SI",
+    "Small Intestine · Intestino Delgado",
+    "back",
+    require("./SI.svg")
+  ),
   makeLayer("BL", "Bladder · Bexiga", "back", require("./BL.svg")),
   makeLayer("KI", "Kidney · Rim", "front", require("./KI.svg")),
   makeLayer("P", "Pericardium · Pericárdio", "front", require("./P.svg")),
-  makeLayer("TW", "Triple Warmer · Triplo Aquecedor", "back", require("./TW.svg")),
+  makeLayer(
+    "TW",
+    "Triple Warmer · Triplo Aquecedor",
+    "back",
+    require("./TW.svg")
+  ),
   makeLayer("GB", "Gallbladder · Vesícula Biliar", "back", require("./GB.svg")),
   makeLayer("LV", "Liver · Fígado", "front", require("./LV.svg")),
   // 2 Vasos Extraordinários
-  makeLayer("GV", "Governing Vessel · Vaso Governador", "back", require("./GV.svg")),
-  makeLayer("CV", "Conception Vessel · Vaso Concepção", "front", require("./CV.svg")),
+  makeLayer(
+    "GV",
+    "Governing Vessel · Vaso Governador",
+    "back",
+    require("./GV.svg")
+  ),
+  makeLayer(
+    "CV",
+    "Conception Vessel · Vaso Concepção",
+    "front",
+    require("./CV.svg")
+  ),
 ];
 
 export const BODY_ATLAS_LAYER_MAP = BODY_ATLAS_LAYERS.reduce<
